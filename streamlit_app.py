6eda2195952a9a79ed780350ddce3308a6fbbfc3
Nimra-Fayaz <142812663+Nimra-Fayaz@users.noreply.github.com>
2024-03-24 14:24:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,6 @@ st.set_page_config(layout="wide")
 genai.configure(api_key="AIzaSyBCL92zWhFPocMHLd2Df2KSPbKWCRCYTSQ") 
 model = genai.GenerativeModel('gemini-1.0-pro')
 
-# Function to fetch top 3 trends for the selected region using Gemini
-def get_top_trends(region):
-    try:
-        prompt = f"Show top 3 trends in {region} which people are searching for, get data from the google trends"
-        response = model.generate_content(prompt)
-        top_trends = [trend.content.parts[0].text for trend in response.candidates]
-        return top_trends
-    except Exception as e:
-        st.error(f"Error fetching trends: {e}")
-        return []
-
 # Main content area
 st.title("Social Media Trends Dashboard")
 
@@ -31,6 +20,7 @@ with st.sidebar:
     # Dropdown for selecting social media platform
     platform = st.selectbox("Select Social Media Platform", ["X", "Instagram", "Facebook", "LinkedIn"])
     generate_post_button = st.button("Generate Post")
+
 
 # Fetch and display top 3 trends for the selected region
 if show_trends_button:
